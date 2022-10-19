fb9109c64aecebfd70093bd9bfe35b5a48603cf5
David <David.delrio@alumni.mondragon.edu>
2022-10-19 20:23:44 +0200

    Cambiamos los ficheros setup y reestructuramos el paquete

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 from setuptools import setup
-setup()
+setup(
+    name = 'felisaparser',
+    version = '1.0.0',
+    description = 'This package contains a function to directly parse a excel file into a csv file',
+    author = 'Adrián Martínez, David Del Río',
+    url = 'https://github.com/daviddelriod/felisaparser',
+    keywords = 'xlsx parser',
+    install_requires = [
+        'pandas',
+        ],
+)
