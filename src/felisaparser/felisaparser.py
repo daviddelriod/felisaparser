40150048b0d4e48d1777dff442ad43f733365494
David <David.delrio@alumni.mondragon.edu>
2022-10-26 16:12:22 +0200

    Actualizamos

--- src/felisaparser/felisaparser.py
+++ src/felisaparser/felisaparser.py
@@ -1,9 +1,18 @@
-import pandas as pd
 class parser:
-    def __init__(self, path):
+    def __init__(self, path, sep=','):
+        self.sep=sep
         self.path = path
 
     def excel_to_csv(self):
+        try:
+            import sys
+            import pandas as pd
+        except ImportError:
+            print("pandas is not installed. Please install it and try again.")
+            sys.exit(1)
         df = pd.read_excel(self.path)
-        df.to_csv(str(self.path[:-5])+'.csv', index=False)
-        print("Done!")
+        if self.path.endswith('.xlsx'):
+            self.path = self.path[:-5]
+        if self.path.endswith('.xls'):
+            self.path = self.path[:-4]        
+        df.to_csv(self.path + '.csv', index=False, sep=self.sep)
